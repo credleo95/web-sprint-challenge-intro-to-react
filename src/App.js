dfcc107095c14e28a82a98699a55ff18b9e3da91
Omarius Credle <ocredle@omariuss-mbp.home>
2021-02-08 23:27:57 -0500

    completed project. Fixed API Call. I initialized state to empty string. This cause a multitude of issues. Initilaized state with an empty array, and styled the character components.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Character from './components/Character'
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [characters, setCharacters] = useState('')
+const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -18,6 +18,7 @@ const [characters, setCharacters] = useState('')
 useEffect(() => {
 axios.get(`${peopleUrl}`)
 .then((response) => {
+  // console.log(response.data.results); 
   setCharacters(response.data);
 })
 .catch((error) => {
